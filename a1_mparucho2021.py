f2c24b873f6e3b6b170a675f83901a6b2ad4f0cb
MarcoParucho <125199963+MarcoParucho@users.noreply.github.com>
2023-06-04 16:01:31 -0400

    FINISHED GAME
    The game has been finished. It should not be possible to beat. MiniMax algorithm has been implemented. Did a little of a presentation.

--- a1_mparucho2021.py
+++ a1_mparucho2021.py
@@ -1,46 +1,25 @@
-"""
-    Author: Marco Parucho
-    Date: 5/20/2023
-    Description: This is Project 1: Tic-Tac-Toe. 
-    In this tic tac toe app I have to implement the MINMAX algorithm...
-"""
 #Globals
 board = [1,2,3,4,5,6,7,8,9]
 currentPlayer = 'X'
+
 # This function creates the game board using a list 
 def drawBoard():
     print(board[0],board[1],board[2])
     print(board[3],board[4],board[5])
     print(board[6],board[7],board[8])
 
-# This function places the 'X' or 'O' in the board
-def place():
-    global currentPlayer #I forgot that we had to do this in Python, help from Stackoverflow, document link on report. 
+# This function places the 'X' or 'O' in the board. Giving it parameters player and position
+def place(player, position):
+    global currentPlayer
     
-    #Adding a while to this function to make sure that if the user enters a choice that is not a number the program will ask them to enter another input
-    while True:
-        choice = input("Enter choice: ")
-        if choice.isdigit():
-            choice = int(choice)
-            if 1 <= choice <= 9:
-                if board[choice-1] != 'X' and board[choice-1] != 'O':
-                    board[choice-1] = currentPlayer
-                    if currentPlayer == 'X':
-                        currentPlayer = 'O'
-                    else:
-                        currentPlayer = 'X'
-                    break
-                else:
-                    print("Error. Position is already filled.")
-            else:
-                print("Error. Pick a choice between 1 and 9!")
-        else:
-            print("Error. Enter a valid numeric choice")
+    board[position-1] = player
+    if currentPlayer == 'X':
+        currentPlayer = 'O'
+    else:
+        currentPlayer = 'X'
 
-
-#this function will be in charge of checking if any player win
-def checkWin():
-    #crearing a list to check for win in a more efficiente way
+# This function checks if any player wins. Passing board as a parameter
+def checkWin(board):
     combinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],  # horizontal
         [0, 3, 6], [1, 4, 7], [2, 5, 8],  # vertical
@@ -49,57 +28,87 @@ def checkWin():
 
     for i in combinations:
         if all(board[pos] == 'X' for pos in i):
-            print("Player X wins the game!")
-            return True
+            return 'X'
         if all(board[pos] == 'O' for pos in i):
-            print("Player O wins the game!")
-            return True
-#I am making this function because I need to check if the game is tied, it is also an important part of the minmax algorithm #learnt about isinstance in a reddit post, link will be documented
-def checkDraw():
-    if all(isinstance(pos, str) for pos in board):
-        print("The game has ended in a tie...")
-        return True
-    return False
+            return 'O'
+    return None
 
-#this function is in charge of the MINMAX process
-def minimax(board, depth, maxPlayer):
-    if checkWin():
-        #returning -1, 0, 1 will allow the algorithm determine if the play is the correct choice to make
-        if currentPlayer == 'X':
-            return -1
-        else:
-            return 1
-    elif checkDraw():
+# This function checks if the game is tied
+def checkDraw(board):
+    return all(isinstance(pos, str) for pos in board)
+
+# Implementing minmax algorithm
+def minmax(board, depth, maxPlayer):
+    if checkWin(board) == 'X':
+        return -1
+    elif checkWin(board) == 'O':
+        return 1
+    elif checkDraw(board):
         return 0
-    #this is going to be the winner
-    if maxPlayer:
-        score = float('-inf')
-        for i in range(len(board)):
-            if isinstance(board[i], int):
-                board[i] = 'O' #places the O for the "AI"
-                x = minimax(board, depth + 1, False)
-                board[i]=i+1
-                score = max(x, score)
-        return score
-    else: #this is going to be the player
-        score = float('inf')
-        for i in range(len(board)):
-            if isinstance(board[i], int):
-                board[i]='X'
-                x = minimax(board, depth+1, True)
-                board[i] = i + 1
-                score = min(x, score)
-        return score
 
-#calling functions
+    if maxPlayer:
+        scoreUp = float('-inf')
+        for i in range(9):
+            if isinstance(board[i], int):
+                board[i] = 'O'
+                eval = minmax(board, depth+1, False)
+                board[i] = i+1
+                if eval is not None:
+                    scoreUp = max(scoreUp, eval)
+        if scoreUp == float('-inf'):
+            return 0
+        return scoreUp
+    else:
+        scoreDown = float('inf')
+        for i in range(9):
+            if isinstance(board[i], int):
+                board[i] = 'X'
+                eval = minmax(board, depth+1, True)
+                board[i] = i+1
+                if eval is not None:
+                    scoreDown = min(scoreDown, eval)
+        if scoreDown == float('inf'):
+            return 0
+        return scoreDown
+
+# This function determines the computer moves
+def bot():
+    score = float('-inf')
+    move = None
+
+    for i in range(9):
+        if isinstance(board[i], int):
+            board[i] = 'O'
+            x = minmax(board, 0, False)
+            board[i] = i+1
+            if x > score:
+                score = x
+                move = i+1
+    place('O', move)
+
+# Main game loop
 while True:
     drawBoard()
-    place()
-    if checkWin():
+    if currentPlayer == 'X':
+        choice = int(input("Enter choice: "))
+        if choice < 1 or choice > 9:
+            print("Error. Pick a digit between 1 and 9")
+            continue
+        if board[choice-1] != choice:
+            print("Error. Position is filled.")
+            continue
+        place('X', choice)
+    else:
+        bot()
+
+    if checkWin(board):
+        drawBoard()
+        print("The computer has won the game!")
         break
-    if checkDraw():
+    elif checkDraw(board):
+        drawBoard()
+        print("TIE!")
         break
-    
 
 
      
