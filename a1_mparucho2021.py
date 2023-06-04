03892bb4ec117778e6f9cb315156b67b236cb492
MarcoParucho <125199963+MarcoParucho@users.noreply.github.com>
2023-06-04 16:26:33 -0400

    Finished game
    For whatever reason last commit did not save the changes I made... a new level of fear has been unlocked.

--- a1_mparucho2021.py
+++ a1_mparucho2021.py
@@ -1,17 +1,28 @@
-#Globals
-board = [1,2,3,4,5,6,7,8,9]
+"""
+    Author: Marco Parucho
+    Date: 5/20/2023
+    Description: This is Project 1: Tic-Tac-Toe. 
+    In this tic tac toe app I have to implement the MINMAX algorithm...
+"""
+import os #importing library to be able to clear board 
+# Globals
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 currentPlayer = 'X'
 
-# This function creates the game board using a list 
+# This function creates the game board using a list
 def drawBoard():
-    print(board[0],board[1],board[2])
-    print(board[3],board[4],board[5])
-    print(board[6],board[7],board[8])
+    os.system("cls")  # Clear the screen
+    print("\t  TIC TAC TOE\n")
+    print("\t", board[0], " | ", board[1], " | ", board[2])
+    print("\t---------------")
+    print("\t", board[3], " | ", board[4], " | ", board[5])
+    print("\t---------------")
+    print("\t", board[6], " | ", board[7], " | ", board[8])
 
 # This function places the 'X' or 'O' in the board. Giving it parameters player and position
 def place(player, position):
     global currentPlayer
-    
+
     board[position-1] = player
     if currentPlayer == 'X':
         currentPlayer = 'O'
@@ -89,27 +100,33 @@ def bot():
 # Main game loop
 while True:
     drawBoard()
+    # The game will only ask for input when the player is 'X'
     if currentPlayer == 'X':
-        choice = int(input("Enter choice: "))
-        if choice < 1 or choice > 9:
-            print("Error. Pick a digit between 1 and 9")
-            continue
-        if board[choice-1] != choice:
-            print("Error. Position is filled.")
-            continue
-        place('X', choice)
+        choice = input("\n\tEnter choice: ")
+        if choice.isdigit():
+            choice = int(choice)
+            if choice < 1 or choice > 9:
+                print("Error. Pick a digit between 1 and 9")
+                continue
+            if board[choice-1] != choice:
+                print("Error. Position is filled.")
+                continue
+            place('X', choice)
+        else:
+            print("ERROR... numeric value must be entered")
     else:
         bot()
 
     if checkWin(board):
         drawBoard()
+        os.system("cls")
         print("The computer has won the game!")
         break
     elif checkDraw(board):
         drawBoard()
+        os.system("cls")
         print("TIE!")
         break
-
 
      
 
